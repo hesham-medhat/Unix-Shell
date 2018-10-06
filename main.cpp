9bbc94251369b636206a4cebd75ef4b5e19794c0
Hesham Medhat <heshammedhat5@gmail.com>
2018-10-06 09:07:49 +0200

    Implement arguments preparation and command execution

--- main.cpp
+++ main.cpp
@@ -32,15 +32,41 @@ void executeCommand(char* line) {
     vector<char*> argsVector = parseArgs(line);
     if (argsVector.empty()) {
         printErrorMessage("No command passed.");
-    } else if (argsVector.size() == 1) {
-        if (fork() != 0) wait(nullptr);
-        else execvp(argsVector.at(0), &argsVector.at(0));
     } else {
-       if (argsVector.at(argsVector.size() - 1) == "&") {
+        if (strcmp(argsVector.at(argsVector.size() - 1), "&") == 0) {
+            /* Prepare arguments */
+            argsVector.at(argsVector.size() - 1) = nullptr;
+            char* args[argsVector.size()];
+            for (int i = 0; i < argsVector.size(); i++) args[i] = argsVector[i];
 
-       }
-        // TODO: Execute the command
+            if (fork() != 0) { // Parent process continues
+                cout << "Running in background" << endl;
+            } else {
+                if (fork() != 0) { // Process termination handler
+                    wait(0);
+                    cout << "Process terminated" << endl; // TODO: Log in file
+                    exit(-1);
+                } else {
+                    execvp(args[0], args);
+                    // Only returns on failure
+                    printErrorMessage("Failed to execute command.");
+                    exit(-1);
+                }
+            }
+        } else {
+            /* Prepare arguments */
+            argsVector.push_back(nullptr);
+            char* args[argsVector.size()];
+            for (int i = 0; i < argsVector.size(); i++) args[i] = argsVector[i];
 
+            if (fork() != 0) wait(0);
+            else {
+                execvp(args[0], args);
+                // Only returns on failure
+                printErrorMessage("Failed to execute command.");
+                exit(-1);
+            }
+        }
     }
 }
 
